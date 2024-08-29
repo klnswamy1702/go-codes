f6e971a874159f5fcd84ac334866ec1cd18815ed
Karampuri Laxmi Narsimha Swamy <lnswamykarampuri@gmail.com>
2024-08-29 10:19:49 +0530

    go codes

---
+++ 26goroutines/main.go
@@ -0,0 +1,84 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+// 	"sync"
+// )
+
+// var signals = []string{"test"}
+
+// var wg sync.WaitGroup //pointer
+// var mut sync.Mutex    // pointer
+
+// func main() {
+// 	// go greeter("Hello")
+// 	// greeter("world")
+// 	websitelist := []string{
+// 		"https://lco.dev",
+// 		"https://go.dev",
+// 		"https://google.com",
+// 		"https://fb.com",
+// 		"https://github.com",
+// 	}
+
+// 	for _, web := range websitelist {
+// 		go getStatusCode(web)
+// 		wg.Add(1)
+// 	}
+
+// 	wg.Wait()
+// 	fmt.Println(signals)
+// }
+
+// // func greeter(s string) {
+// // 	for i := 0; i < 6; i++ {
+// // 		time.Sleep(3 * time.Millisecond)
+// // 		fmt.Println(s)
+// // 	}
+// // }
+
+// func getStatusCode(endpoint string) {
+// 	defer wg.Done()
+
+// 	res, err := http.Get(endpoint)
+
+// 	if err != nil {
+// 		fmt.Println("OOPS in endpoint")
+// 	} else {
+// 		mut.Lock()
+// 		signals = append(signals, endpoint)
+// 		mut.Unlock()
+
+// 		fmt.Printf("%d status code for %s\n", res.StatusCode, endpoint)
+
+// 	}
+// }
+
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func f(from string) {
+    for i := 0; i < 3; i++ {
+        fmt.Println(from, ":", i)
+    }
+}
+
+func main() {
+
+    f("direct")
+
+    go f("goroutine")
+
+    go func(msg string) {
+        fmt.Println(msg)
+    }("going")
+
+    time.Sleep(time.Second)
+    fmt.Println("done")
+}
---
+++ 28channels/go.mod
@@ -0,0 +1,5 @@
+module channels
+
+go 1.17
+
+require golang.org/x/crypto v0.26.0 // indirect
